cd213dad525e23da34ba58ed09d7f72b5ea36cf7
Like Sha <tradersalike88@yahoo.com>
2020-03-01 13:38:50 -0800

    JumpTOTheWindow

--- src/test/java/com/automation/tests/day2/JumpToTheNewWindow.java
+++ src/test/java/com/automation/tests/day2/JumpToTheNewWindow.java
@@ -45,4 +45,19 @@ public class JumpToTheNewWindow {
 
     }
 
+    /**
+     * This method helps to switch in between windows based on page title
+     * @param pageTitle
+     * @param driver
+     */
+    public static void switchTwoWindowsBasedOnTitle(String pageTitle, WebDriver driver){
+        Set<String> windows = driver.getWindowHandles();
+        for(String window: windows){
+            driver.switchTo().window(window);
+            if(driver.getTitle().equals(pageTitle)){
+                break;
+            }
+        }
+    }
+
 }
