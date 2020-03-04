0497b258ffd72b71531fc67b74e979a1de6088e2
Like Sha <tradersalike88@yahoo.com>
2020-03-03 16:57:33 -0800

    DriverFactory

--- src/test/java/com/automation/tests/day3/FindElementsPractice.java
+++ src/test/java/com/automation/tests/day3/FindElementsPractice.java
@@ -1,14 +1,36 @@
 package com.automation.tests.day3;
 
+import com.automation.utilities.DriverFactory;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class FindElementsPractice {
 
+//    /**
+//     * //we are writing same codes again and again
+//     * //we will write a method and call it : DRY Principle
+//     * //sth that support our code : utilities
+//     * //under automation package
+//     * //We created utilities package and DriverFactory class under automation package
+//     * //we wrote reusable methods inside DriverFactory class named => createDriver
+//     * //whenever we need to crate webDriver object
+//     * //we will call our custom method
+//     * /*
+//     * WebDriverManager.chromedriver().setup();
+//     * WebDriver driver = new ChromeDriver();
+//     *  */
+//     *
+//     */
     public static void main(String[] args) {
-        WebDriverManager.chromedriver().setup();
-        WebDriver driver =new ChromeDriver();
+//        WebDriverManager.chromedriver().setup();
+//        WebDriver driver =new ChromeDriver();
+
+        WebDriver driver = DriverFactory.createDriver("chrome");
+        driver.get("http://practice.cybertekschool.com/sign_up");
+
+
+        driver.quit();//to close everything
 
 
     }
--- src/test/java/com/automation/utilities/DriverFactory.java
+++ src/test/java/com/automation/utilities/DriverFactory.java
@@ -7,13 +7,14 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class DriverFactory {
     /**
+     * You homework will be improve this method
      * This method return webdriver object based on browser type
      * If you want to use chrome browser, just provide chrome as a parameter
      * @param browserName
      * @return webdriver object
      */
 
-    public static WebDriver createADriver(String browserName){
+    public static WebDriver createDriver(String browserName){
         if(browserName.equalsIgnoreCase("chrome")){
             WebDriverManager.chromedriver().setup();
             return new ChromeDriver();
