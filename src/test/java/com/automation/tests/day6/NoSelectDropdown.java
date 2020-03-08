00628c75ff513e19ef2fc748473218ecefd2daf2
Like Sha <tradersalike88@yahoo.com>
2020-03-08 10:36:10 -0700

    NoSelectDropdown

--- src/test/java/com/automation/tests/day6/NoSelectDropdown.java
+++ src/test/java/com/automation/tests/day6/NoSelectDropdown.java
@@ -4,6 +4,9 @@ import com.automation.utilities.BrowserUtils;
 import com.automation.utilities.DriverFactory;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
 
 public class NoSelectDropdown {
 
@@ -15,9 +18,22 @@ public class NoSelectDropdown {
         BrowserUtils.wait(3);
         driver.findElement(By.id("dropdownMenuLink")).click();//to expand dropdown
 
-     // <a class="dropdown-item" href="https://www.google.com/">Google</a>
-        BrowserUtils.wait(2);
-        driver.findElement(By.linkText("Amazon")).click();//click on option
+        //<a class="dropdown-item" href="https://www.amazon.com/">Amazon</a>
+        BrowserUtils.wait(3);
+
+        List<WebElement> allLinks = driver.findElements(By.className("dropdown-item"));
+        for (WebElement link: allLinks){
+            System.out.println(link.getText()+" : "+link.getAttribute("href"));
+        }
+
+
+
+
+
+        driver.findElement(By.linkText("Etsy")).click();//click on option
+
+        BrowserUtils.wait(3);
+        driver.quit();
 
 
 
