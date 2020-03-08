04bcbc1bad922770614d8dc20d99265588bd2e23
Like Sha <tradersalike88@yahoo.com>
2020-03-08 10:20:54 -0700

    NoSelectDropdown

--- src/test/java/com/automation/tests/day6/SelectByTextMultipleOptions.java
+++ src/test/java/com/automation/tests/day6/SelectByTextMultipleOptions.java
@@ -4,7 +4,10 @@ import com.automation.utilities.BrowserUtils;
 import com.automation.utilities.DriverFactory;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
+
+import java.util.List;
 
 public class SelectByTextMultipleOptions {
 
@@ -20,6 +23,21 @@ public class SelectByTextMultipleOptions {
         languagesSelect.selectByVisibleText("Java");
         languagesSelect.selectByVisibleText("JavaScript");
         languagesSelect.selectByVisibleText("Python");
+
+        //let's get all selected options
+        List<WebElement> selectedLanguages = languagesSelect.getAllSelectedOptions();
+
+        for(WebElement selectedLanguage:selectedLanguages){
+            System.out.println(selectedLanguage.getText());
+        }
+
+        BrowserUtils.wait(3);
+
+        languagesSelect.deselectByVisibleText("Java");//to unselect something
+
+        BrowserUtils.wait(3);
+        languagesSelect.deselectAll();
+
 
 
 
