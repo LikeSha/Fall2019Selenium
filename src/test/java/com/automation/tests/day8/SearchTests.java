355e2cf812d6962d9589cb5e03a459cc329e5393
Like Sha <tradersalike88@yahoo.com>
2020-03-14 13:06:10 -0700

    SearchTests

--- src/test/java/com/automation/tests/day8/SearchTests.java
+++ src/test/java/com/automation/tests/day8/SearchTests.java
@@ -2,6 +2,7 @@ package com.automation.tests.day8;
 
 import com.automation.utilities.BrowserUtils;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.apache.commons.io.input.BrokenInputStream;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
@@ -23,7 +24,7 @@ public class SearchTests {
         driver.get("http://google.com");
         driver.findElement(By.name("q")).sendKeys("java", Keys.ENTER);
         BrowserUtils.wait(2);
-        //since very search item has a tag name <h3>
+        //since every search item has a tag name <h3>
         //it's the easiest way to collect all of them
         List<WebElement> searchItems = driver.findElements(By.tagName("h3"));
         for(WebElement searchItem : searchItems){
@@ -50,11 +51,40 @@ public class SearchTests {
      * then user clicks on the search button
      * and user clicks on the first search item
      * and user verifies that title of the search item contains "Java"
+     * above are test scenarios we made
      */
     @Test(description = "Search for Java book on amazon")
                 public void amazonSearchTest(){
-                    driver.get("http://amazon.com");
+        driver.get("http://amazon.com");
+        //there is a chance that item is not visible
+        //so we need to maximize window before clicking on it
+        driver.manage().window().maximize();
 
+        BrowserUtils.wait(5);
+
+        driver.findElement(By.id("twotabsearchtextbox")).sendKeys("Java", Keys.ENTER);
+        BrowserUtils.wait(5);
+        //find all links inside h2 elements, because h2 element is no clickable itself
+        //hyperlinks must be clickable
+        List<WebElement> searchItems = driver.findElements(By.xpath("//h2//a"));
+
+        //click on the first item
+        for(WebElement searchItem: searchItems){
+            System.out.println("Title: "+searchItem.getText());
+        }
+        searchItems.get(0).click();
+        BrowserUtils.wait(5);
+
+        WebElement productTitle = driver.findElement(By.id("title"));
+        String productTitleString = productTitle.getText();
+        System.out.println(productTitleString);
+
+        Assert.assertTrue(productTitleString.contains("Java"));
+
+        //so h2 elements are not clickable, even though they contain links
+        //that's why, instead of collection all h2 elements
+        //we collected all hyperlinks
+        //every hyperlink represent some search item
                 }
 
 
