33304df58863175d5757a2c0ff99f0161bfb1fa4
Like Sha <tradersalike88@yahoo.com>
2020-03-15 10:13:10 -0700

    vytrackloninPageTest

--- src/test/java/com/automation/tests/vytrack/login/loginPageTests.java
+++ src/test/java/com/automation/tests/vytrack/login/loginPageTests.java
@@ -1,9 +1,14 @@
 package com.automation.tests.vytrack.login;
 
+import com.automation.utilities.BrowserUtils;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 //STATIC IMPORT OF ALL ASSERTIONS
 import static org.testng.Assert.*;
@@ -16,6 +21,42 @@ public class loginPageTests {
     // https--data encrypted ,no change for hackers to retrieve info
     //http-- data as a plain text, very easy to hack it
     private String URL = "https://qa2.vytrack.com/user/login";
+    //credentials for store manager
+    private String username = "storemanager85";
+    private String password = "UserUser123";
+    private By usernameBy = By.id("prependedInput");
+    private By passwordBy = By.id("prependedInput2");
+    //private By usernameBy =By.id("prependedInput"); //By is a class--> when we find an element by using By.... methods
+    ////it returns By data type and we stored it in to By as above statement.
+    private By warningMessageBy = By.cssSelector("[class='alert alert-error'] > div");
+
+    @Test(description = " verify that warning message displays when user enters invalid username")
+    public void invalidUsername(){
+          driver.findElement(usernameBy).sendKeys("invalidusername");
+          driver.findElement(passwordBy).sendKeys("UserUser123", Keys.ENTER);
+        BrowserUtils.wait(3);
+        WebElement warningElement = driver.findElement(warningMessageBy);
+        assertTrue(warningElement.isDisplayed());
+
+        String expected = "Invalid user name or password.";
+        String actual = warningElement.getText();
+        assertEquals(actual,expected);
+    }
+
+    @Test(description = "Login as store manager and verify that title is equals to Dashboard")
+    public void loginAsStoreManager(){
+        driver.findElement(usernameBy).sendKeys(username);
+        driver.findElement(passwordBy).sendKeys(password,Keys.ENTER);
+        BrowserUtils.wait(5);
+
+        String expected = "Dashboard";
+        String actual = driver.getTitle();
+
+
+    }
+
+
+
 
     @BeforeMethod
     public void setup(){
