f15d4c8178ee48e85efadd6f831407dde7891f3c
Like Sha <tradersalike88@yahoo.com>
2020-03-24 16:40:41 -0700

    ConfigurationProperties

--- src/test/java/com/automation/tests/day12/SystemProperties.java
+++ src/test/java/com/automation/tests/day12/SystemProperties.java
@@ -6,7 +6,15 @@ public class SystemProperties {
 
     @Test
     public void test(){
+       //
         System.out.println(System.getProperty("user.dir"));
+        System.out.println(System.getProperty("os.name"));
+
+        String pathToDownLoads = System.getProperty("user.home")+"/Downloads";
+
+        System.out.println(pathToDownLoads);
+
+        System.out.println(System.getProperty("os.arch"));
     }
 
 }
