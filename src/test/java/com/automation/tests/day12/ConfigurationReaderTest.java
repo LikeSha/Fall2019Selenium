1768d1e67dac53fa08ad10d9ad206bc88257ef36
Like Sha <tradersalike88@yahoo.com>
2020-03-24 17:27:05 -0700

    ConfigurationReader

--- src/test/java/com/automation/tests/day12/ConfigurationReaderTest.java
+++ src/test/java/com/automation/tests/day12/ConfigurationReaderTest.java
@@ -1,5 +1,6 @@
 package com.automation.tests.day12;
 
+import com.automation.utilities.ConfigurationReader;
 import org.testng.annotations.Test;
 
 public class ConfigurationReaderTest {
@@ -7,6 +8,7 @@ public class ConfigurationReaderTest {
 
     @Test
     public void readProperties(){
-
+       String browser = ConfigurationReader.getProperty("browser");
+        System.out.println(browser);
     }
 }
