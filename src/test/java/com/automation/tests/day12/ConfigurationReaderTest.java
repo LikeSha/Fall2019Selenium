e1b521ef7b08931e221d60d3a29a2ab8519beb80
Like Sha <tradersalike88@yahoo.com>
2020-03-24 18:30:51 -0700

    Driver

--- src/test/java/com/automation/tests/day12/ConfigurationReaderTest.java
+++ src/test/java/com/automation/tests/day12/ConfigurationReaderTest.java
@@ -1,6 +1,7 @@
 package com.automation.tests.day12;
 
 import com.automation.utilities.ConfigurationReader;
+
 import org.testng.annotations.Test;
 
 public class ConfigurationReaderTest {
@@ -9,6 +10,20 @@ public class ConfigurationReaderTest {
     @Test
     public void readProperties(){
        String browser = ConfigurationReader.getProperty("browser");
+       String url = ConfigurationReader.getProperty("qa1");
         System.out.println(browser);
+        System.out.println(url);
+
+        String storeManager = ConfigurationReader.getProperty("store_manager");
+        String password = ConfigurationReader.getProperty("password");
+        String driver = ConfigurationReader.getProperty("driver");
+
+        System.out.println(storeManager);
+
+        System.out.println(password);
+
+        System.out.println(driver);
+
+
     }
 }
