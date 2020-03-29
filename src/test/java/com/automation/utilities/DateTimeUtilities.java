b8ce8c7f7eef55632d83e285de49c379a55bc454
Like Sha <tradersalike88@yahoo.com>
2020-03-29 12:01:04 -0700

    DateTimeUtilities

---
+++ src/test/java/com/automation/utilities/DateTimeUtilities.java
@@ -0,0 +1,25 @@
+package com.automation.utilities;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class DateTimeUtilities {
+
+    /**
+     * This method returns current date as a String
+     * Provide a format as a parameter
+     *
+     * MM---to specify month line : 01,02,03,
+     * MMM---  Jan, Feb, Mar....
+     *
+     * dd--to specify day, like 01,02,03
+     *
+     * yyyy==to specify year ,like 2010, 2020
+     * @param format for example MMM dd, yyyy =Mar 29,2020
+     * @return current date as a String
+     *https://www.journaldev.com/17899/java-simpledateformat-java-date-format
+     */
+    public static String getTodaysDate(String format){
+        return LocalDate.now().format(DateTimeFormatter.ofPattern(format));
+    }
+}
