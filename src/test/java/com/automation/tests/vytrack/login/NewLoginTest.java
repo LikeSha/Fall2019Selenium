6647fee71cf392fcaf902ea1739a66ef62d414c5
Like Sha <tradersalike88@yahoo.com>
2020-03-31 18:20:14 -0700

    NewLoginTest

--- src/test/java/com/automation/tests/vytrack/login/NewLoginTest.java
+++ src/test/java/com/automation/tests/vytrack/login/NewLoginTest.java
@@ -15,9 +15,15 @@ public class NewLoginTest extends AbstractTestBase {
      */
      @Test
     public void verifyPageTitle(){
+         //test is coming from ExtentTest
+         //we must add to every test at the beginning
+         //test = report.createTest("Test name");
+         test = report.createTest("Verify page title");
          LoginPage loginPage = new LoginPage();
          loginPage.login();
          Assert.assertEquals(Driver.getDriver().getTitle(), "Dashboard");
+         //if assertion passed , it wil set test status in report to passed
+         test.pass("Page title Dashboard was verified");
 
      }
 
