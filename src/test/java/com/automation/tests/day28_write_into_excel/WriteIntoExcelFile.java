68934f9f6111b9bc3f954b18a1bc511464cc2861
Like Sha <tradersalike88@yahoo.com>
2020-04-14 17:47:33 -0700

    method changed

--- src/test/java/com/automation/tests/day28_write_into_excel/WriteIntoExcelFile.java
+++ src/test/java/com/automation/tests/day28_write_into_excel/WriteIntoExcelFile.java
@@ -19,21 +19,24 @@ public class WriteIntoExcelFile {
 
         Sheet sheet= workbook.getSheet("QA3-short");
         Row row = sheet.getRow(1);//second row
-        Cell cell = row.getCell(row.getLastCellNum()-1);//last column
+        Cell cell = row.getCell(5);//create new cell
 
         System.out.println("Before : " + cell.getStringCellValue());
         cell.setCellValue("PASSED");//I am changing from n/a to passed
         System.out.println("After : " + cell.getStringCellValue());
 
         Row firstRow = sheet.getRow(0);//get first row
-        Cell newCell = firstRow.createCell(row.getLastCellNum());//CREATE NEW CELL
+        Cell newCell = firstRow.createCell(6);//CREATE NEW CELL
         newCell.setCellValue("Date of execution");//give the name to this cell
 
         //write date and time info into second row, last column
 
         Row secondRow = sheet.getRow(1);
-        Cell newCell2 = secondRow.createCell(row.getLastCellNum());
-        newCell2.setCellValue(LocalDate.now());//I will set current date and time info into new cell
+        Cell newCell2 = secondRow.createCell(6);
+        newCell2.setCellValue(LocalDate.now().toString());//I will set current date and time info into new cell
+
+        //whenever you create a cell, it does not mean that all cell under neat will created automatically
+       //So first we created object of second row then we created new cell and write date and time information
 
 
         //I create if I want to write something into the file
