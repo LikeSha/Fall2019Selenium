4e0c4a31c67082ebde241003ba00de1ddd142e03
Like Sha <tradersalike88@yahoo.com>
2020-06-27 22:58:01 -0700

    SeleniumO class vytrack LoginPageTests updated code note

--- src/test/java/com/automation/tests/vytrack/fleet/VehiclesPageTests.java
+++ src/test/java/com/automation/tests/vytrack/fleet/VehiclesPageTests.java
@@ -18,11 +18,11 @@ public class VehiclesPageTests {
     /**
      * under fleet package create a class called VehiclesPageTests
      *
-     *  in this class. you will need to create @beforemethod with setup and @aftermethod with teadown part.
+     *  in this class. you will need to create @BeforeMethod with setup and @AfterMethod with tearDown part.
      *  Use LoginPageTests class as a reference.
      *
      * create a test called verifyPageSubTitle
-      -- in this test , you will need to navigate to Fleet ---->Vehicles and verify that page subtitleis equals to "All Cars"
+      -- in this test , you will need to navigate to Fleet ---->Vehicles and verify that page subtitle is equals to "All Cars"
      *
      *   user assertions for validation.
      */
@@ -34,6 +34,8 @@ public class VehiclesPageTests {
     private By usernameBy = By.id("prependedInput");
     private By passwordBy = By.id("prependedInput2");
     private By fleetBy = By.xpath("//span[@class='title title-level-1' and contains(text(),'Fleet')]");
+    //or like this : private By fleetBy = By.xpath("//*[@class='title title-level-1' and contains(text(),'Fleet')]");
+    //* stands for any tag name .
     private By subtitleBy = By.className("oro-subtitle");
     private By pageNumberBy = By.cssSelector("input[type='number']");
 
@@ -63,7 +65,7 @@ public class VehiclesPageTests {
         WebElement subTitleElement = driver.findElement(subtitleBy);
         System.out.println(subTitleElement.getText());
 
-        String expected = "All Cars";
+        String expected = "Cars";
         String actual = subTitleElement.getText();
 
         Assert.assertEquals(actual,expected);
@@ -94,7 +96,7 @@ public class VehiclesPageTests {
 
     @BeforeMethod
     public void setup() {
-        WebDriverManager.chromedriver().version("79").setup();
+        WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         driver.get(URL);
         driver.manage().window().maximize();
