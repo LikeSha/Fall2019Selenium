99cc923471485f1784bd916af518004cc8ef5d4e
Like Sha <tradersalike88@yahoo.com>
2020-07-12 14:47:35 -0700

    Selenium class day12 SystemProperties updated code note

--- src/test/java/com/automation/tests/day12/SystemProperties.java
+++ src/test/java/com/automation/tests/day12/SystemProperties.java
@@ -6,10 +6,11 @@ public class SystemProperties {
 
     @Test
     public void test(){
-       //
+       //System.getProperty("user.dir")+"/pom.xml"
         System.out.println(System.getProperty("user.dir"));
         System.out.println(System.getProperty("os.name"));
 
+        //flexible path to downloads folder
         String pathToDownLoads = System.getProperty("user.home")+"/Downloads";
 
         System.out.println(pathToDownLoads);
