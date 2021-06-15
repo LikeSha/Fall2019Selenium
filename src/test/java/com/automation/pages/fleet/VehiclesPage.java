bd4ab1c5335081ec138b6c13025652e3b4f61bc6
Like Sha <tradersalike88@yahoo.com>
2021-06-15 09:49:39 -0700

    AbstractPageBase classes note from sublime.

--- src/test/java/com/automation/pages/fleet/VehiclesPage.java
+++ src/test/java/com/automation/pages/fleet/VehiclesPage.java
@@ -7,8 +7,7 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class VehiclesPage extends AbstractPageBase {
-    // we extends AbstractPageBase ,what it give us ? First , PageFactory initialized ! then navigations available,
-    // it inherited all attributes and behavior of AbstractPageBase class.
+
     @FindBy(partialLinkText = "Create Car")
     private WebElement createCar;
 
@@ -17,6 +16,13 @@ public class VehiclesPage extends AbstractPageBase {
         wait.until(ExpectedConditions.elementToBeClickable(createCar)).click();
     }
 
+    // we extends AbstractPageBase ,what it give us ? First , PageFactory initialized ! then navigations available,
+    // it inherited all attributes and behavior of AbstractPageBase class.
+
+// why do we need this method in this page ? why we not directly made this webelement available in the test class and
+    //click on it ? the reason is very simple : because of wait issue. we need to take care of this wait issue
+    // in this page class ,because this vytrack application has a lot of problems. we can provide additional
+    // wait to support finding webelement.
 }
 
 /*
