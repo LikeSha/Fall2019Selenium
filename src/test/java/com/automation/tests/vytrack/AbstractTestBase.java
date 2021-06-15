842f922dba99453fd0cd5c7f45ad91bc1992214b
Like Sha <tradersalike88@yahoo.com>
2021-06-14 17:08:28 -0700

    AbstractTestBase classes note from sublime.

--- src/test/java/com/automation/tests/vytrack/AbstractTestBase.java
+++ src/test/java/com/automation/tests/vytrack/AbstractTestBase.java
@@ -67,7 +67,7 @@ public abstract class AbstractTestBase {
     @BeforeMethod
     public void setup(){
         String URL = ConfigurationReader.getProperty("qa3");
-        Driver.getDriver().get(URL);
+        Driver.getDriver().get(URL);// get() method is coming from WebDriver();
         Driver.getDriver().manage().window().maximize();
         wait = new WebDriverWait(Driver.getDriver(),15);
         actions = new Actions(Driver.getDriver());
